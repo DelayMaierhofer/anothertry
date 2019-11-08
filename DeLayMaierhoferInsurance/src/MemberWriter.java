73ea71fe880a5ae9c5520eb5eab271b266952d98
Dr. Ray Klump <klumpra@lewisu.edu>
2019-11-08 15:21:12 -0600

    Fixed conflict

--- DeLayMaierhoferInsurance/src/MemberWriter.java
+++ DeLayMaierhoferInsurance/src/MemberWriter.java
@@ -2,10 +2,7 @@
 //DeLay & Maierhofer
 //Another comment
 //And another
-=======
-//Test
 
->>>>>>> 1e9f2e86c9355b660709db4ca223e9f11f2d0487
 import java.util.ArrayList;
 import java.util.Scanner; 
 
